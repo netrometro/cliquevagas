6edd7111fd9bb4eb478db96888d018e90acb67ab
Higor Blandes <higorblandes@gmail.com>
2020-11-17 15:46:38 -0300

    Update install.php

--- install.php
+++ install.php
@@ -1,12 +1,12 @@
 <?php 
 /* SOLICITANDO ARQUIVO DE CONEXÃO COM O BANCO */
 require("db.php");
-/* DELETAR TABELA CASO EXISTER usuario */
-banco("DROP TABLE IF EXISTS usuario");
+/* DELETAR TABELA CASO EXISTER empresa */
+banco("DROP TABLE IF EXISTS empresa");
 /* PRINTAR SE A TABELA FOI APAGADA */
-echo "<p>Tabela usuário apagada com sucesso !</p>";
+echo "<p>Tabela Empresa apagada com sucesso !</p>";
 /* CRIAR TABELA E POPULAR COM VALORES */
-$sql ="CREATE TABLE usuario(";
+$sql ="CREATE TABLE empresa(";
 $sql .= "codigo SERIAL NOT NULL,";
 $sql .= "nome VARCHAR(250),";
 $sql .= "email VARCHAR(100),";
@@ -15,12 +15,12 @@ $sql .= "PRIMARY KEY(codigo)";
 $sql .= ");";
 /* PASSANDO O PARAMETRO SQL PARA FUNÇÃO BANCO */
 banco($sql);
-echo "<p>Tabela usuário criada com sucesso !</p>";
-$sql = "INSERT INTO usuario (nome, email, senha) ";
-$sql .= "VALUES ('Fulano de Tal', 'fulano@email.com', '1');";
-$sql .= "INSERT INTO usuario (nome, email, senha) ";
-$sql .= "VALUES ('Cicrano da Silva', 'cicrano@email.com', '2');";
+echo "<p>Tabela Empresa criada com sucesso !</p>";
+$sql = "INSERT INTO empresa (nome, email, senha) ";
+$sql .= "VALUES ('SENAC Caruaru', 'caruaru@senac.com', '1');";
+$sql .= "INSERT INTO empresa (nome, email, senha) ";
+$sql .= "VALUES ('SENAC Recife', 'recife@senac.com', '2');";
 /* PASSANDO O PARAMETRO SQL PARA FUNÇÃO BANCO E POPULANDO A TABELA */
 banco($sql);
-echo "<p>Tabela usuário populada com sucesso ! </p>";
+echo "<p>Tabela Empresa populada com sucesso ! </p>";
 ?>
