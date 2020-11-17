3043144d1e4a8c9f0900d9488cf92bc331b5d01d
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2020-11-17 17:18:51 -0300

    Add files via upload

---
+++ html/teladevagas.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clique Vagas Caruaru</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="teladevagas.css" />
+  </head>
+  <body class="bg2">
+    <header>
+    <div class="titulo bg1"><img src="images/logo.png" /> Click Vagas Caruaru</div>  
+    <div class="armario">
+      <div class="gaveta">
+        <form action='#' method='post'>
+          <div class="caixatexto">
+            <input type="text" placeholder="Procure sua vaga" />
+            <button type="submit"><i class="material-icons">search</i></button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </header>
+
+    <div class="container card">
+      <h3>SENAC</h3>
+
+      <h4>Auxiliar de Eletricista, Instalador Fotovoltáico.</h4>
+
+      <p>Auxiliar de eletricista experiência de 1 ano, ter curso
+        de eletricista predial e disponibilidade para trabalhar
+        durante a madrugada.<br/>
+        Instador fotovotáico, curso de eletrotécnica e 
+        experiência de 2 anos.</p>
+
+      <p>Enviar currículo para eletrica220v.caruaru@gmail.com</p>
+
+    </div>
+
+  </body>
+</html>
