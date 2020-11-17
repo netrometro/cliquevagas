e20a2b3991d80647d332058195879618bdda7e22
Higor Blandes <higorblandes@gmail.com>
2020-11-17 16:30:16 -0300

    Criando tabela Anuncios

--- install.php
+++ install.php
@@ -23,4 +23,24 @@ $sql .= "VALUES ('SENAC Recife', 'recife@senac.com', '2');";
 /* PASSANDO O PARAMETRO SQL PARA FUNÇÃO BANCO E POPULANDO A TABELA */
 banco($sql);
 echo "<p>Tabela Empresa populada com sucesso ! </p>";
+/* DELETAR TABELA CASO EXISTER anuncios */
+banco("DROP TABLE IF EXISTS anuncios");
+/* PRINTAR SE A TABELA FOI APAGADA */
+echo "<p>Tabela Anuncios apagada com sucesso !</p>";
+$sql ="CREATE TABLE anuncios(";
+$sql .="codigo SERIAL NOT NULL,";
+$sql .="precisase VARCHAR(250),";
+$sql .="descricao VARCHAR(400),";
+$sql .="telefone VARCHAR(100),";
+$sql .="email VARCHAR(100),";
+$sql .="endereco VARCHAR(100),";
+$sql .="tempo DATE NOT DEFAULT CURRENT_DATE";
+$sql .=");";
+/* PASSANDO O PARAMETRO SQL PARA FUNÇÃO BANCO */
+banco($sql);
+echo "<p>Tabela Anuncios criada com sucesso !</p>";
+$sql ="INSERT INTO anuncios (precisase, descricao, telefone, email, endereco, tempo) ";
+$sql .="VALUES ('Barbeiro', 'Precisa-se de barbeiro experiente.', '8198772364', 'barbershop@barber.com', 'Rua campo novo 199', '2020-10-23');";
+banco($sql);
+echo "<p>Tabela Anuncios populada com sucesso ! </p>";
 ?>
