a8e5502ef1a183b68d929e1204e496c99088e4fa
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-11-19 19:45:15 -0300

    Update pesquisa.php

--- pesquisa.php
+++ pesquisa.php
@@ -64,13 +64,13 @@ if (isset($resultado)) {
   while ($dados = pg_fetch_row($resultado))
 ?>
     <div class="container card">
-      <h3><?= $dados['empresa'] . ?></h3>
-      <h4><?= $dados['precisase'] . ?></h4>
-      <p><?= $dados['descricao'] . ?></p>
+      <h3><?= $dados['empresa'] ?></h3>
+      <h4><?= $dados['precisase'] ?></h4>
+      <p><?= $dados['descricao'] ?></p>
       <p><strong>Contato:</strong></p>
-      <p><?= $dados['telefone'] . ?></p>
-      <p><?= $dados['endereco'] . ?></p>
-      <p><?= $dados['email'] . ?></p>
+      <p><?= $dados['telefone'] ?></p>
+      <p><?= $dados['endereco'] ?></p>
+      <p><?= $dados['email'] ?></p>
 
     </div>
     <? pg_close($conn); ?>
