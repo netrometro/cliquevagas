f1a9e8d4b65b0e171fd24fe8ba52fcf89ca31762
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-11-20 00:11:58 -0300

    Corrigindo bug de syntaxe
    Bug de syntaxe sendo corrigido

--- controllers/pesquisar_anuncio.php
+++ controllers/pesquisar_anuncio.php
@@ -4,7 +4,7 @@ if (!isset($_POST) or $_POST['pesquisa'] == "") {
   header("Location: ../pesquisa.php");
 }
 $pesquisa = $_POST['pesquisa'];
-$sql = 'SELECT * FROM anuncios WHERE precisase LIKE "%' . $pesquisa . '%";';
+$sql = 'SELECT * FROM anuncios WHERE precisase LIKE "%'.$pesquisa.'%";';
 $resultado = banco($sql);
 session_start();
 $_SESSION['resultado'] = $resultado;
