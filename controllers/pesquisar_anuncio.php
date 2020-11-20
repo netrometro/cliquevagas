542269f2dae982be12da13db56beb0bae3b2284a
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-11-20 00:03:55 -0300

    Corrigindo bug de syntax

--- controllers/pesquisar_anuncio.php
+++ controllers/pesquisar_anuncio.php
@@ -1,20 +1,12 @@
 <?php
 require('../model/persistency/db.php');
-
-
 $pesquisa = $_POST['pesquisa'];
-
 if (!isset($_POST) or $_POST['pesquisa'] == "") {
   header("Location: ../pesquisa.php");
-  $sql = 'SELECT * FROM anuncio WHERE precisase LIKE "%' . $pesquisa . '%";';
 }
-
+$sql = 'SELECT * FROM anuncio WHERE precisase LIKE "%' . $pesquisa . '%";';
 $resultado = banco($sql);
-
-
-
 session_start();
 $_SESSION['resultado'] = $resultado;
-
 header("Location: ../pesquisa.php");
 ?>
