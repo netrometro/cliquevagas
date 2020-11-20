caf3ecff45d83941a339192883867a3f3c0db6d7
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-11-20 14:35:56 -0300

    Ajustes na index e pesquisa

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+if (isset($_SESSION['resultado']) and $_SESSION['resultado'] != "") {
+  $resultado = $_SESSION['resultado'];
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -8,7 +15,9 @@
     <link rel="stylesheet" href="styles/style.css" />
   </head>
   <body class="bg2">
-  <a href="controllers/install.php">Clique aqui para "ligar o banco"</a>
+
+    <a href="controllers/install.php">Clique aqui para "ligar o banco"</a>
+
     <header>
     <div class="titulo bg1"><img src="images/logo.png" /> Click Vagas Caruaru</div>  
     <div class="armario">
@@ -16,17 +25,38 @@
         <p class="mensagem">Encontre uma oportunidade de emprego.</p>
       </div>
       <div class="gaveta">
-        <form action='controllers/resultadoBusca.php' method='post'>
+        <form action='controllers/pesquisa_anuncio.php' method='post'>
           <div class="caixatexto">
             <input name="busca" type="text" placeholder="Procure sua vaga" />
             <button type="submit"><i class="material-icons">search</i></button>
           </div>
         </form>
       </div>
+
+<?php
+if (isset($resultado)) { ?>
       <div class="gaveta">
         <a class="botao bg3" href="#">Entrar como empresa</a>
         <a class="botao bg4" href="#">Cadastrar empresa</a>
       </div>
+<?php } else { ?>
+
+  <div class="container card">
+      <h3>SENAC</h3>
+
+      <h4>Auxiliar de Eletricista, Instalador Fotovoltáico.</h4>
+
+      <p>Auxiliar de eletricista experiência de 1 ano, ter curso de eletricista predial e disponibilidade para trabalhar durante a madrugada. Instador fotovotáico, curso de eletrotécnica e experiência de 2 anos.</p>
+
+      <p><strong>Contato<strong></p>
+      <p>site<br />
+      eletrica220v.caruaru@gmail.com<br />
+      Endereço<br />
+      Telefone</p>
+
+    </div>
+
+<?php } ?>
     </div>
     </header>
   </body>
