4ebc565e51da1850957bb2ea4dc420f02681e01d
Higor Blandes <higorblandes@gmail.com>
2020-11-23 16:44:49 -0300

    Update cadastrar_empresa.php
    Correção de syntax

--- model/cadastrar_empresa.php
+++ model/cadastrar_empresa.php
@@ -4,17 +4,16 @@ require("persistency/db.php");
 echo $_POST['empresa'];
 echo $_POST['email'];
 echo $_POST['senha'];
-
-if (!isset($_POST['empresa']) && $_POST['empresa'] == "" && 
-    !isset($_POST['email']) && $_POST['email'] == ""  && 
-    !isset($_POST['senha']) && $_POST['senha'] == "" ) {
-        $nome = $_POST['empresa'];
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+if (isset($_POST['submit'])) {
+    $nome = $_POST['empresa'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
     $sql = 'INSERT INTO empresa (nome, email, senha)';
     $sql .= "VALUES ('$nome', '$email', '$senha');";
+    if($sql){
+        echo "Dados gravado com sucesso.";
+    }
     banco($sql);
-    
     //header("Location: ../listaranuncio.php ")
   }
     //header("Location: ../empresa.html")
