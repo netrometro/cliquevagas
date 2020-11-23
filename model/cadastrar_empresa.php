4814c0f7062cfae19ec8e4537e2ad166a2d6c500
Higor Blandes <higorblandes@gmail.com>
2020-11-23 17:10:42 -0300

    Corrigindo código
    Corrigindo syntax de código

--- model/cadastrar_empresa.php
+++ model/cadastrar_empresa.php
@@ -1,20 +1,11 @@
 <?php 
 /* SOLICITANDO ARQUIVO DE CONEXÃO COM O BANCO */
 require("persistency/db.php");
-print_r ($_POST);
-if (isset($_POST['submit'])) {
     $nome = $_POST['empresa'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    echo $nome;
-    echo $email;
-    echo $senha;
     $sql = "INSERT INTO empresa (nome, email, senha)";
     $sql .= " VALUES ('$nome', '$email', '$senha');";
-    echo $sql;
     $resultado = banco($sql);
-    echo $resultado;
-    //header("Location: ../listaranuncio.php ")
-  }
-    //header("Location: ../empresa.html")
+    header("Location: ../listaranuncio.php ")
 ?>
