1d84f0e13a0f985dbded6c32b6c363f4ebd5caf6
Higor Blandes <higorblandes@gmail.com>
2020-11-27 17:23:55 -0300

    Criando salvar_pesquisa.php

--- model/salvar_pesquisa.php
+++ model/salvar_pesquisa.php
@@ -1,18 +1,20 @@
 <?php
 require('persistency/db.php');
-
-
-
-echo "Salvar pesquisa";
-echo $_POST['email'];
-echo $_POST['pesquisa'];
-
+if (isset($_POST['email']) && $_POST['email'] != "" && isset($_POST['busca']) && $_POST['busca'] != "" ) {
+    $email = pg_escape_string($_POST['email']);
+    $busca = pg_escape_string($_POST['busca']);
+    
+    $resultado = banco($sql);
+if($resultado){
+    $sql = "UPDATE favbusca WHERE email='" . $email . "' SET buscaFavorita='" . $busca . "'" ;
+}else
+    $sql = "INSERT INTO favbusca VALUES (email, buscaFavorita) " ;
+}
 // Verfica se recebe email e pesquisa
 // Procura no banco o email
 // se achar
 //    update (atualiza) o email encontrado com a nova busca
-// senão achar
+// senão achar 
 //    insert email e busca
-
 header("Location: ../pesquisa_salva.html");
 ?>
