806263bf6314f6a7b676db063fc8dd1524a710cd
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-11-27 17:16:09 -0300

    Página perfil.php com css

--- perfil.php
+++ perfil.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clique Vagas Caruaru</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="style/principal.css" />
+    <link rel="stylesheet" href="style/botoes.css" />
+    <link rel="stylesheet" href="style/card.css" />
   </head>
   <body class="bg2">
     <div class="container">
@@ -13,42 +16,47 @@
       <a class="limpartitulo" href="index.php"> Click Vagas Caruaru</a>
       </div>
       <div class="conteudo">
-      <a class="botao bg4" href="#">Anunciar</a>
+      <a class="bt_principal" href="#">Anunciar</a>
       
       <div class="resultado">
-          <div class="container card">
-            <p>Vencimento 0</p>
-            <h3>......</h3>
-
-            <h4>.....</h4>
-            
-            <p>..... </p>
-
-            <p><strong>Contato:</strong></p>
-            
-            <p>caruaru@gmail.com</p>
-            <p>site</p>
-            <p>telefone</p>
-            <p>endereço</p>
-
+          <div class="card">
+            <div class="col c1">
+              <a href="#"><i class="material-icons">edit</i></a>
+            </div>
+            <div class="col c2">
+              <p>SENAC</p>
+              <p>27/11/2020</p>
+              <p>Descrição...</p>
+              <p><strong>Contato:</strong></p>
+              <p>caruaru@gmail.com</p>
+              <p>site</p>
+              <p>telefone</p>
+              <p>endereço</p>
+            </div>
+            <div class="col c3">
+              <a href="#"><i class="material-icons">cancel</i></a>
+            </div>
           </div>
 
-          <div class="container card inativo">
-            <p>Vencimento 0</p>
-            <h3>......</h3>
-
-            <h4>.....</h4>
-            
-            <p>..... </p>
-
-            <p><strong>Contato:</strong></p>
-            
-            <p>caruaru@gmail.com</p>
-            <p>site</p>
-            <p>telefone</p>
-            <p>endereço</p>
-
+          <div class="card inativo">
+            <div class="col c1">
+              <a href="#"><i class="material-icons">edit</i></a>
+            </div>
+            <div class="col c2">
+              <p>SENAC</p>
+              <p>27/11/2020</p>
+              <p>Descrição...</p>
+              <p><strong>Contato:</strong></p>
+              <p>caruaru@gmail.com</p>
+              <p>site</p>
+              <p>telefone</p>
+              <p>endereço</p>
+            </div>
+            <div class="col c3">
+              <a href="#"><i class="material-icons">cancel</i></a>
+            </div>
           </div>
+
         </div>
 
       </div>
