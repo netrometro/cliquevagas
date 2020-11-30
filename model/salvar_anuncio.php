f487ffe8b01a2101d16375191a9f7afbfec60a32
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-11-30 16:35:02 -0300

    Modificação login.php para guardar o codigo

--- model/salvar_anuncio.php
+++ model/salvar_anuncio.php
@@ -1,4 +1,12 @@
 <?php
+// require autentication
 
+// verifica se foram enviados os post
+
+// pega na session o cógigo
+
+// salva no banco o anuncio
+
+// redireciona para a página de perfil
 
 ?>
