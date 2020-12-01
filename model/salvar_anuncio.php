fe30a7141ff0205d831a543d79a29298e4240497
Demóstenes Filho <tecnologiasone@gmail.com>
2020-12-01 16:57:15 -0300

    add site variavel no salvar salvar anuncio

--- model/salvar_anuncio.php
+++ model/salvar_anuncio.php
@@ -7,6 +7,7 @@ if (isset($_POST['precisase']) && $_POST['precisase'] != "" ) {
     $descricao = pg_escape_string($_POST['descricao']);
     $telefone = pg_escape_string($_POST['telefone']);
     $email = pg_escape_string($_POST['email']);
+    $site = pg_escape_string($_POST['site']);
     $endereco = pg_escape_string($_POST['endereco']);
     $datainsercao = pg_escape_string($_POST['data_insercao']);
     $datavencimento = pg_escape_string($_POST['data_vencimento']);
@@ -14,8 +15,8 @@ if (isset($_POST['precisase']) && $_POST['precisase'] != "" ) {
     session_start();
     $codigoempresa = $_SESSION['usuario'];
    
-   $sql ="INSERT INTO anuncios (codigo_empresa, precisase, descricao, telefone, email, endereco, data_insercao, data_vencimento) ";
-   $sql .="VALUES ($codigoempresa, '$precisase', '$descricao', '$telefone', '$email', '$endereco', '$datainsercao', '$datavencimento');";
+   $sql ="INSERT INTO anuncios (codigo_empresa, precisase, descricao, telefone, email, site, endereco, data_insercao, data_vencimento) ";
+   $sql .="VALUES ($codigoempresa, '$precisase', '$descricao', '$telefone', '$email', '$site', '$endereco', '$datainsercao', '$datavencimento');";
    $resultado = banco($sql);
 
    header("Location: ../perfil.php");
