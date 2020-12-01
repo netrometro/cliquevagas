8758ab4ca5564ee6d50858f965b0a3afe9563512
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-12-01 15:51:51 -0300

    A página de perfil agora mostra o nome da empresa logada

--- perfil.php
+++ perfil.php
@@ -1,5 +1,12 @@
 <?php 
 require("controllers/autentication.php");
+
+require("model/persistency/db.php");
+
+$sql = "SELECT nome FROM empresa WHERE codigo=" . $_SESSION['usuario'];
+$resultado = banco($sql);
+$resultado = pg_fetch_assoc($resultado);
+$empresa = $resultado['nome'];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -19,6 +26,7 @@ require("controllers/autentication.php");
       <a class="limpartitulo" href="index.php"> Click Vagas Caruaru</a>
       </div>
       <div class="conteudo">
+      <h3 class="empresa"><?= $empresa ?></h3>
       <a class="bt_principal" href="adicionaranuncio.php">Anunciar</a>
       
       <div class="resultado">
@@ -43,7 +51,7 @@ require("controllers/autentication.php");
 
           <div class="card inativo">
             <div class="col c1">
-              <a href="#"><i class="material-icons">edit</i></a>
+              <a href="model/cancelar_anuncio.php?codigo=  <?= 1 ?>"><i class="material-icons">edit</i></a>
             </div>
             <div class="col c2">
               <p>SENAC</p>
