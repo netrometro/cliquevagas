5775f8311d51396f5f52099cd4651c600583e929
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-12-01 22:30:11 -0300

    Corrigindo lógica do código

--- model/salvar_pesquisa.php
+++ model/salvar_pesquisa.php
@@ -1,21 +1,15 @@
 <?php
 require('persistency/db.php');
-
+$email = pg_escape_string($_POST['email']);
+$pesquisa = pg_escape_string($_POST['pesquisa']);
 if (isset($_POST['email']) && $_POST['email'] != "" && isset($_POST['pesquisa']) && $_POST['pesquisa'] != "" ) {
-    $email = pg_escape_string($_POST['email']);
-    $pesquisa = pg_escape_string($_POST['pesquisa']);
-
-    // tem que fazer uma busca aqui
-    $sql = "SELECT * FROM favbusca WHERE email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'" ;
-    
-    $resultado = banco($sql);
-
+     $sql = "SELECT * FROM favbusca WHERE email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'" ;
+     $sql = "UPDATE IF EXISTS email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'"; 
+     $resultado = banco($sql);
     if($resultado){
-        $sql = "UPDATE favbusca SET buscaFavorita='" . $pesquisa . "' WHERE email='" . $email . "' ";
-    }else {
-        $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
+   }else {
+    $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
     }
-    
     $resultado = banco($sql);
 }
 header("Location: ../pesquisa_salva.html");
