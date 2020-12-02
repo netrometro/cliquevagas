77bf71abdf6174a72f1d932bfc3be5988622ca63
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-12-02 16:00:25 -0300

    Corrigindo salvar_pesquisa.php

--- model/salvar_pesquisa.php
+++ model/salvar_pesquisa.php
@@ -3,13 +3,21 @@ require('persistency/db.php');
 $email = pg_escape_string($_POST['email']);
 $pesquisa = pg_escape_string($_POST['pesquisa']);
     if (isset($_POST['email']) && $_POST['email'] != "" && isset($_POST['pesquisa']) && $_POST['pesquisa'] != "" ) {
-        $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
+        $sql = "SELECT * FROM favbusca WHERE email ILIKE '%" . $_POST["email"] . "%'";
         $resultado = banco($sql);
      if($resultado){
-        header("Location: ../pesquisa_salva.html");
-        //$sql = "UPDATE IF EXISTS email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'";      
+        $sql = "UPDATE INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
+        $resultado = banco($sql);
+        header("Location: ../pesquisa_salva.html");     
         }else {
-            echo "<p>Erro !</p>"; //
+            $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
+            $resultado = banco($sql);
+            header("Location: ../pesquisa_salva.html");
                 }
+}else{
+    echo "<p>Erro !</p>";
 }
 ?>
+
+
+       
