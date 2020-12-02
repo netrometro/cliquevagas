bb4ced1d8d6791d333e006145128041e3832fb4f
Higor Blandes <67286451+higorblands@users.noreply.github.com>
2020-12-01 22:40:44 -0300

    Corrigindo lógica do código

--- model/salvar_pesquisa.php
+++ model/salvar_pesquisa.php
@@ -4,13 +4,13 @@ $email = pg_escape_string($_POST['email']);
 $pesquisa = pg_escape_string($_POST['pesquisa']);
 if (isset($_POST['email']) && $_POST['email'] != "" && isset($_POST['pesquisa']) && $_POST['pesquisa'] != "" ) {
      $sql = "SELECT * FROM favbusca WHERE email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'" ;
-     $sql = "UPDATE IF EXISTS email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'"; 
+     $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
      $resultado = banco($sql);
     if($resultado){
-        $sql = "INSERT INTO favbusca (email, buscaFavorita) VALUES ('" . $email . "', '" . $pesquisa . "')" ;
+        $sql = "UPDATE IF EXISTS email='" . $email . "' AND buscaFavorita='" . $pesquisa . "'"; 
         header("Location: ../pesquisa_salva.html");
    }else {
-    echo "<p>Erro ! </p>";
+    echo "<p>Erro !</p>";
     }
     $resultado = banco($sql);
 }
