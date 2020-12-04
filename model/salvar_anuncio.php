b1308b95e89c6ffa6ae1b69b52e889917dd064b2
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-12-04 15:32:12 -0300

    algoritmo de envio de e-mail

--- model/salvar_anuncio.php
+++ model/salvar_anuncio.php
@@ -21,10 +21,12 @@ if (isset($_POST['precisase']) && $_POST['precisase'] != "" ) {
    $sql .="VALUES ($codigoempresa, '$precisase', '$descricao', '$telefone', '$email', '$site', '$endereco', '$datainsercao', '$datavencimento');";
    $resultado = banco($sql);
 
+    // faz uma pesquisa em favbusca para todos os e-mail que salvaram a busca (%LIKE%)
+    // Apenas, se tiver ao menos um resultado faz...
+    // Entra em um laço de repetição
+    // Envia um e-mail para cada e-mail do resultado
+
    header("Location: ../perfil.php");
-
 }
-
 //header("Location: ../erroformulario.html");
-
 ?>
