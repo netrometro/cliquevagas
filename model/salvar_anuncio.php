e890ce517aacd63ca92c4c12267336b54d0d1485
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-12-07 14:46:55 -0300

    varios

--- model/salvar_anuncio.php
+++ model/salvar_anuncio.php
@@ -16,17 +16,32 @@ if (isset($_POST['precisase']) && $_POST['precisase'] != "" ) {
     $datavencimento = date('Y/m/d', strtotime($datainsercao . ' + ' . $dias .' days'));
 
     $codigoempresa = $_SESSION['usuario'];
-   
-   $sql ="INSERT INTO anuncios (codigo_empresa, precisase, descricao, telefone, email, site, endereco, data_insercao, data_vencimento) ";
-   $sql .="VALUES ($codigoempresa, '$precisase', '$descricao', '$telefone', '$email', '$site', '$endereco', '$datainsercao', '$datavencimento');";
+   /*
+   $sql  = "INSERT INTO anuncios (codigo_empresa, precisase, descricao, telefone, email, site, endereco, data_insercao, data_vencimento) ";
+   $sql .= "VALUES ($codigoempresa, '$precisase', '$descricao', '$telefone', '$email', '$site', '$endereco', '$datainsercao', '$datavencimento');";
    $resultado = banco($sql);
-
+*/
     // faz uma pesquisa em favbusca para todos os e-mail que salvaram a busca (%LIKE%)
+    $sql = "SELECT * FROM favbusca";
+    $resultado = banco($sql);
+
+    $emails = array();
+    echo $precisase . '<br />';
+    while ($linha = pg_fetch_assoc($resultado)) {
+        echo $linha['buscafavorita'] . '<br />';
+        echo strpos($precisase, $linha['buscafavorita']) . '<br />';
+        $pos = strpos($precisase, $linha['buscafavorita']);
+        if ($pos instanceof undefined)
+            array_push($emails, $linha['email']);
+    }
+
+    print_r($emails);
+
     // Apenas, se tiver ao menos um resultado faz...
     // Entra em um laço de repetição
     // Envia um e-mail para cada e-mail do resultado
 
-   header("Location: ../perfil.php");
+   //header("Location: ../perfil.php");
 }
 //header("Location: ../erroformulario.html");
 ?>
