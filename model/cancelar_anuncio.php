d32e9648bdda8daa210a35a3ecd664264b009960
demostenesfilho <tecnologiasone@gmail.com>
2020-12-10 10:41:44 -0300

    cancelar anuncio sem testar

--- model/cancelar_anuncio.php
+++ model/cancelar_anuncio.php
@@ -4,11 +4,18 @@
   require("persistency/db.php");
   // recebe por get o código do anúncio a ser cancelado
   if (isset($_GET['codigo']) && $_GET['codigo'] != "" ) {
-    
+    $chave = pg_escape_string($_POST['chave']);
+
   // se existir
   //    faz o update da data final do anúncio para o dia de ontem
+  $datainsercao = date("Y/m/d");
+  $datavencimento = date('Y/m/d', strtotime($datainsercao . ' - ' . $dias .' days'));
+
+  $sql = "UPDATE empresa SET data_insercao = "$datainsercao", data_vencimento = "$datavencimento" WHERE chave = '$chave'";
+  $resultado = banco($sql);  
+
   //    redireciona para a página de perfil
-    
+  header('Location: ../perfil.php'); 
   }
 
 ?>
